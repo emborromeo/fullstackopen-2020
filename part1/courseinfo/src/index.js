e52021f064ff43ae57ea896935de691aa39e35a6
emborromeo <ericaborromeo06@gmail.com>
2020-10-09 20:12:36 +0800

    exercise 1.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 const Header=(props)=>{
   return(
    <div>
@@ -14,9 +13,10 @@ const Header=(props)=>{
    console.log(props)
    return(         
      <div>
-     <Part part={props.part1}/>
-     <Part part={props.part2}/>
-     <Part part={props.part3}/>
+     <Part part={props.parts[0]}/>
+     <Part part={props.parts[1]}/>
+     <Part part={props.parts[2]}/>
+    
      </div>            
    )
  }
@@ -33,30 +33,31 @@ const Header=(props)=>{
  const Total=(props)=>{
    return(
      <div>
-       <p>Number of exercises {props.exercises} </p>
+       <p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises} </p>
      </div>
    )
  }
  const App = () => {
    const course = 'Half Stack Application Development'
-   const part1 ={
+   const parts =[
+   {
      name: 'Fundamentals of React',
      exercises : 10
-   }
-   const part2 ={ 
+   },
+   { 
      name:'Using props to pass data',
      exercises: 7
-  }
-   const part3 = {
+  },
+   {
      name: 'State of a component',
      exercises : 14
    }
- 
+  ]
    return (
      <div>
        <Header course={course} />
-       <Content part1={part1} part2={part2} part3={part3}/>
-       <Total exercises={part1.exercises+part2.exercises+part3.exercises}/>
+       <Content parts={parts}/>
+       <Total parts={parts}/>
      </div>
    )
  }
@@ -66,3 +67,4 @@ ReactDOM.render(<App />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
