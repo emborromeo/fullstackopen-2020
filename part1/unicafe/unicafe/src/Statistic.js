97eafed5c74c76ce437d2bb8db9003f171489372
emborromeo <ericaborromeo06@gmail.com>
2020-10-16 20:52:32 +0800

    Exercise 10

---
+++ part1/unicafe/unicafe/src/Statistic.js
@@ -0,0 +1,11 @@
+import React from 'react' //state hook
+  
+
+export const Statistic = ({ text, value }) => {
+  return (
+    <div>
+      {text} {value}
+    </div>
+
+  );
+};
--- part1/unicafe/unicafe/src/index.js
+++ part1/unicafe/unicafe/src/index.js
@@ -1,28 +1,46 @@
 import React, { useState } from 'react' //state hook
 import ReactDOM from 'react-dom';
 import { Button } from './Button';
-import { Statistics } from './Statistics';
+import { Statistic } from './Statistic';
+
 
 const Header=({text})=><h1>{text}</h1>
+
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <div>No Feedback given</div>
+    );
+  }
+  return (
+    <div>
+    <Statistic text="good" value={props.good}/>
+    <Statistic text="neutral" value={props.neutral}/>
+    <Statistic text="bad" value={props.bad}/>
+    <Statistic text="all" value={props.all}/>
+    <Statistic text="average" value={props.average}/>
+    <Statistic text="positive" value={props.positive}/>
+   
+  </div>
+  )
+
+}
 const App=()=>{
   
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all= good+neutral+bad;
+  const all= good+neutral+bad
 
-const goodReview = () =>setGood(good + 1)
-const neutralReview = () =>setNeutral(neutral + 1)
-const badReview = () =>setBad(bad + 1)
-
+ 
 
 return(
 <div>
     <Header text='give feedback'/>
 
-  <Button onClick={goodReview} text='good'/>
-  <Button onClick={neutralReview} text='neutral'/>
-  <Button onClick={badReview} text='bad'/>
+  <Button onClick={()=> {setGood(good+1)}} text='good'/>
+  <Button onClick={()=>{setNeutral(neutral+1)}} text='neutral'/>
+  <Button onClick={()=>{setBad(bad+1)}} text='bad'/>
   <Header text='statistics'/>
   <Statistics good={good} neutral={neutral} bad={bad} all={all} average={(good-bad)/all} positive={(good*100)/all + '%'}/>
  
